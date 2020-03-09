eff0afdc6e78d6b6b1833e243b8a105109dc2b35
zeged <zeged@yahoo.com>
2020-03-09 07:51:37 +0200

    new main activity

---
+++ android/android-tutorial-1/src/org/freedesktop/gstreamer/tutorials/tutorial_1/MainActivity.java
@@ -0,0 +1,30 @@
+package org.freedesktop.gstreamer.tutorials.tutorial_1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.btn_main).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this, "this is a test", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(MainActivity.this, Tutorial1.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
