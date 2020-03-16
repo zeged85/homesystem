5bffe16510359398bb79db2012e7a0f56fa632a9
zeged <zeged@yahoo.com>
2020-03-17 01:10:05 +0200

    TcpClient added

--- android/android-tutorial-1/src/org/freedesktop/gstreamer/tutorials/tutorial_1/Login.java
+++ android/android-tutorial-1/src/org/freedesktop/gstreamer/tutorials/tutorial_1/Login.java
@@ -1,7 +1,13 @@
 package org.freedesktop.gstreamer.tutorials.tutorial_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.freedesktop.gstreamer.tutorials.tutorial_1.TcpClient;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +15,43 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        setButton1();
+
     }
+
+
+
+    public void setButton1(){
+        findViewById(R.id.login_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //IP
+                EditText ip_et = (EditText) findViewById(R.id.ip_txt);
+                String IP = ip_et.getText().toString();
+
+                //PORT
+                EditText port_et = (EditText)findViewById(R.id.port_txt);
+                Integer PORT = Integer.parseInt(port_et.getText() .toString());
+
+
+
+                String login_msg = "trying to login to " + IP + ":" + PORT;
+                Toast.makeText(Login.this,login_msg , Toast.LENGTH_LONG).show();
+
+
+                TcpClient tcpClient = new TcpClient(IP,PORT);
+
+
+//                Intent intent = new Intent(Login.this, Tutorial1.class);
+//                startActivity(intent);
+//
+
+            }
+        });
+    }
+
+
 }
