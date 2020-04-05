c1255a799d85ba06ad2b31a31ae672bab2e82b04
zeged <zeged85@gmail.com>
2020-04-05 11:09:50 -0700

    sending files

--- Server/MessageHandler.py
+++ Server/MessageHandler.py
@@ -1,6 +1,7 @@
 import asyncio
 import socket
 import os # filesize
+#import tqdm
 
 
 async def handleDataReceived(self,loop,data):
@@ -20,7 +21,7 @@ def blocking_io(self):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     try:
-        server_address = ('10.0.0.41', 0)
+        server_address = ('10.0.0.41', 38901)
         print("starting up on ",server_address[0]," port ",server_address[1])
         sock.bind(server_address)
     except socket.error as msg:
@@ -48,18 +49,23 @@ def blocking_io(self):
         connection, client_address = sock.accept()
         print("connection from:", client_address)
 
+        #hello_msg = b"welcome client!\n"
+        #connection.sendall(hello_msg)
 
-        data = f.read(100)
+        data = f.read(1024)
         #print(data)
 
 
-
+        i=0
         while data:
         #hello_msg = "welcome client!"
+            i+=1
+            print("sending..." + str(i))
             connection.sendall(data)
-            data = f.read(100)
+            data = f.read(1024)
     
     #connection.shutdown(socket.SHUT_WR)
+    print("done.")
     connection.shutdown(2)
     connection.close()
     sock.close()
