cd4cbf27749fa356079781d41f7e015bc5919341
zegged <zeged85@gmail.com>
2021-01-21 03:20:03 +0200

    renamed

--- server/gstreamer/scripts/capture.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gst-launch-1.0 ximagesrc startx=1280 use-damage=0 ! video/x-raw,framerate=30/1 ! videoscale method=0 ! video/x-raw,width=1280,height=1024  ! ximagesink
--- server/gstreamer/scripts/receive.sh
+++
@@ -1,1 +0,0 @@
- gst-launch-1.0 -v udpsrc port=5000 caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! queue ! decodebin ! queue ! videoconvert ! queue ! autovideosink
